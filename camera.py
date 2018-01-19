258e6056d0827838e6a295927a9e6c3086553b15
Joe <joe.bell34@btinternet.com>
2018-01-19 21:42:07 +0000

    I do love partay games

---
+++ camera.py
@@ -0,0 +1,39 @@
+"""camera.py
+A class to constantly take photos, for quik access
+"""
+import threading
+
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+
+class ConstantCamera(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self)
+        self.camera = PiCamera(*args, **kwargs)
+        self._camarray = PiRGBArray(self.camera)
+        self._image = None
+        self._lock = threading.Lock()
+        self._close_event = threading.Event()
+
+    def run(self):
+        # capture frames from the camera
+        for frame in self.camera.capture_continuous(self._camarray, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            with self._lock:
+                # capture_continuous yields buffer array
+                self._image = frame.array
+            
+            # clear the stream in preparation for the next frame
+            self._camarray.truncate(0)
+            if self._close_event.is_set():
+                break
+        self.camera.close()
+
+    def get_image(self):
+        # Avoid catching it halfway thru
+        with self._lock:
+            returnval = self._image
+        # Return appropriate array...
+        return returnval
