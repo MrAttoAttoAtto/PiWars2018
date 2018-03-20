68e1fe48d4c57849b96b2cbea21f598f42e1747d
Atto Allas <atto.allas@gmail.com>
2018-03-20 17:35:38 +0000

    Pls no

--- modes/maze2.py
+++ modes/maze2.py
@@ -35,14 +35,14 @@ class Maze2:
                 self.dprint("Left turn sighted Wall on the left is {} away!".format(distances[0]))
                 self.state = "turning"
                 self.side_dist = distances[0]
+                sleep(0.3)
                 ROBOT.set_tank(-1, 1)
-                sleep(0.2)
             elif distances[2] > MAZE_SIDE_THRESH or distances[2] == 0:
                 self.dprint("Right turn sighted Wall on the right is {} away!".format(distances[2]))
                 self.state = "turning"
                 self.side_dist = distances[2]
+                sleep(0.3)
                 ROBOT.set_tank(1, -1)
-                sleep(0.2)
 
 
             elif 0 < distances[0] < MAZE_CLOSE_THRESH:
@@ -60,8 +60,9 @@ class Maze2:
             #self.dprint("Turning! Waiting for {}, getting {}".format(self.side_dist, distances[1]))
             if distances[1] >= MAZE_PICKINESS:
                 self.dprint("Turning stopped!")
-                ROBOT.set_tank(0.5, 0.5)
+                ROBOT.forwards(speed=0.5)
                 self.state = "forwards"
+                sleep(0.5)
 
 
 
